99c053357e2052e3345014cd45977af879f27c5e
MHShihan <mahmodul.no.1@gmail.com>
2024-05-01 13:12:58 +0600

    Learned typescript object

---
+++ module-1/src/1.4.ts
@@ -0,0 +1,2 @@
+// tuple is one kind of array where order is maintained
+let ageName: [number, string, boolean] = [23, "Shihan", true];
---
+++ module-1/src/1.5.ts
@@ -0,0 +1,17 @@
+// Reference Type ---> Object
+
+const user: {
+  university: "Uttara University"; // type --> literal types
+  firstName: string;
+  middleName?: string; // optional type
+  lastName: string;
+  age: number;
+  isCurrentStudent: boolean;
+} = {
+  university: "Uttara University",
+  firstName: "Mahmudul ",
+  middleName: "Hasan",
+  lastName: "Shihan",
+  age: 23,
+  isCurrentStudent: true,
+};
