788d199d16168606c004eb7251fb00240df6be78
MHShihan <mahmodul.no.1@gmail.com>
2024-05-04 22:59:49 +0600

    Lerned how to create class, parameter type, and instace in ts

---
+++ module3/3.1.ts
@@ -0,0 +1,33 @@
+{
+  // OOP --> Class
+
+  class Animal {
+    // name: string;            //property
+    // species: string;
+    // sound: string;
+
+    // Parameter properties
+    // We can remove duplicate type declaration using parameter properties which is "public"
+    constructor(
+      public name: string,
+      public species: string,
+      public sound: string
+    ) {
+      //   this.name = name;
+      //   this.species = species;
+      //   this.sound = sound;
+    }
+    // We have to use normal function inside a class and "this" does not works on arrow function
+    // a function is called method inside a class
+    makeSound() {
+      console.log(`The ${this.name} says ${this.sound}`);
+    }
+  }
+
+  const dog = new Animal("Germern Shepard", "Animalia", "Ghew Ghew");
+  const cat = new Animal("Ola biral", "animal", "Mew mew");
+
+  dog.makeSound();
+
+  //
+}
