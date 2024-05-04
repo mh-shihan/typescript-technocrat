5a61ee75e348ab6feb78e85d1d1bf69cda3ce8dc
MHShihan <mahmodul.no.1@gmail.com>
2024-05-04 09:38:03 +0600

    Learned Generic type in ts

---
+++ module2/2.3.ts
@@ -0,0 +1,39 @@
+{
+  // Generic type
+
+  type GenericArray<T> = Array<T>;
+
+  const rollNumbers1: number[] = [1, 2, 3];
+  const rollNumbers2: Array<number> = [1, 2, 3];
+  const rollNumbers3: GenericArray<number> = [1, 2, 3];
+
+  const mentors1: string[] = ["Mr.X", "Mr. Y", "Mr.Z"];
+  const mentors2: Array<string> = ["Mr.X", "Mr. Y", "Mr.Z"];
+  const mentors3: GenericArray<string> = ["Mr.X", "Mr. Y", "Mr.Z"];
+
+  const booleanArray1: boolean[] = [true, false, true];
+  const booleanArray2: Array<boolean> = [true, false, true];
+  const booleanArray3: GenericArray<boolean> = [true, false, true];
+
+  const user: GenericArray<{ name: string; age: number }> = [
+    {
+      name: "Shihan",
+      age: 23,
+    },
+    {
+      name: "Razzak",
+      age: 24,
+    },
+  ];
+
+  // Generic Tuple
+  type GenericTuple<X, Y> = [X, Y];
+
+  const human: GenericTuple<string, string> = ["Mr.X", "Ms.Y"];
+  const userWithId: GenericTuple<number, { name: string; email: string }> = [
+    123,
+    { name: "Shihan", email: "s@gmail.com" },
+  ];
+
+  //
+}
