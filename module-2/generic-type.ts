8c22429cd6f4070599e6d4134912b85adfcbfe1c
mh-shihan <mahmodul.no.1@gmail.com>
2025-05-08 22:00:56 +0600

    write code of ts generic type

--- module-2/generic-type.ts
+++ module-2/generic-type.ts
@@ -3,6 +3,14 @@
     return arg;
   }
 
+  const genericType2 = <T>(arg: T): T => arg;
+
   const first = genericType<number>(10);
-  console.log(first);
+  console.log("Number: ", first);
+  console.log("String: ", genericType2("Shihan"));
+
+  type GenericType<T> = Array<T>;
+
+  const nums: GenericType<number> = [1, 2, 3, 4];
+  console.log(nums);
 }
